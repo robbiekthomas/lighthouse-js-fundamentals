a69fc54fbbc32b6e30934ff9277df640855433f2
Robbie Thomas <robbiekt@gmail.com>
2022-12-08 09:31:49 -0800

    lastIndexOf function

---
+++ last-index-of.js
@@ -0,0 +1,11 @@
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+
+function lastIndexOf(array, num) {
+  const revArray = array.reverse();
+  let revIndex = revArray.indexOf(num);
+  return revIndex === -1 ? -1 : array.length - revIndex - 1;
+}
